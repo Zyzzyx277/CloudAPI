b35af75d02fb2e5218251b98c88fd6a660f01c8f
Marvin <marvin.krausser@uni-ulm.de>
2023-09-02 22:14:49 +0200

    added Paths

--- CloudSystem/CloudSystem/Model/FileObject.cs
+++ CloudSystem/CloudSystem/Model/FileObject.cs
@@ -6,13 +6,15 @@ public class FileObject
     public string IdUser { get; set; }
     public string IdFile { get; set; }
     public string Name { get; set; }
+    public string Path { get; set; }
 
-    public FileObject(string content, string idUser, string idFile, string name)
+    public FileObject(string content, string idUser, string idFile, string name, string path)
     {
         Content = content;
         IdUser = idUser;
         IdFile = idFile;
         Name = name;
+        Path = path;
     }
 
     public static HashSet<FileObject> FileDb = new();
